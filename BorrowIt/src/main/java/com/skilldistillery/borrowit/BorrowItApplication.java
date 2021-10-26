c21533a98aa9eb927836df105d1bbc94c151f72a
Khiveley <kenneth.hiveley@gmail.com>
2021-10-26 14:54:02 -0500

    Create Spring Boot REST project

---
+++ BorrowIt/src/main/java/com/skilldistillery/borrowit/BorrowItApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.borrowit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowItApplication.class, args);
+	}
+
+}
