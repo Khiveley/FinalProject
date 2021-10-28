b7cf4d3b50a945284cd5291d26b0c5bd2aa117f8
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-10-28 10:06:16 -0600

    entities green

--- JPABorrowIt/src/main/java/com/skilldistillery/borrowit/entities/User.java
+++ JPABorrowIt/src/main/java/com/skilldistillery/borrowit/entities/User.java
@@ -1,11 +1,9 @@
 package com.skilldistillery.borrowit.entities;
 
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,8 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-
-import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 public class User {
@@ -24,27 +20,9 @@ public class User {
 	
 	private String username;
 	private String password;
-	
-	@Column(name="first_name")
-	private String firstName;
-	
-	@Column(name="last_name")
-	private String lastName;
-	
-	private String email;
-	
-	@CreationTimestamp
-	@Column(name="create_date")
-	private LocalDateTime createDate;
-	
-	@ManyToOne
-	@JoinColumn (name="address_id")
-	private Address address;
-	
 	private Boolean enabled;
 	private String role;
 	
-<<<<<<< HEAD
 	@ManyToOne
 	@JoinColumn(name = "address_id")
 	private Address address;
@@ -52,10 +30,6 @@ public class User {
 	@OneToMany(mappedBy="user")
 	private List<Borrow> borrows;
 	
-=======
-	@Column(name="profile_image")
-	private String profileImage;
->>>>>>> 632106ceceb3b495c973f14083f92d4d294ffca3
 	
 	@OneToMany(mappedBy="user")
 	private List<Rating> ratings;
@@ -103,54 +77,6 @@ public class User {
 
 	public int getId() {
 		return id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public LocalDateTime getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(LocalDateTime createDate) {
-		this.createDate = createDate;
-	}
-
-	public Address getAddress() {
-		return address;
-	}
-
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-
-	public String getProfileImage() {
-		return profileImage;
-	}
-
-	public void setProfileImage(String profileImage) {
-		this.profileImage = profileImage;
 	}
 
 	public void setId(int id) {
@@ -229,7 +155,6 @@ public class User {
 
 	@Override
 	public String toString() {
-<<<<<<< HEAD
 		StringBuilder builder = new StringBuilder();
 		builder.append("User [id=");
 		builder.append(id);
@@ -252,13 +177,4 @@ public class User {
 		builder.append("]");
 		return builder.toString();
 	}  
-=======
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstName=" + firstName
-				+ ", lastName=" + lastName + ", email=" + email + ", createDate=" + createDate + ", enabled=" + enabled
-				+ ", role=" + role + ", profileImage=" + profileImage + "]";
-	}   
-	
-	   
->>>>>>> 632106ceceb3b495c973f14083f92d4d294ffca3
 }
-
