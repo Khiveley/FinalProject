680f988088ac9d8fce7a58249e1baa79102a0ec0
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-10-28 13:34:17 -0600

    Category Classes

--- BorrowIt/src/main/java/com/skilldistillery/borrowit/services/CategoryServiceImpl.java
+++ BorrowIt/src/main/java/com/skilldistillery/borrowit/services/CategoryServiceImpl.java
@@ -1,5 +1,30 @@
 package com.skilldistillery.borrowit.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skilldistillery.borrowit.entities.Category;
+import com.skilldistillery.borrowit.repositories.CategoryRepository;
+
 public class CategoryServiceImpl implements CategoryService {
+
+	@Autowired
+	private CategoryRepository repo;
+	
+	@Override
+	public Category findById(int id) {
+		Optional<Category> op = repo.findById(id);
+		if (op.isPresent()) {
+			return op.get();
+		}
+		return null;
+	}
+
+	@Override
+	public List<Category> showAllCategories() {
+		return repo.findAll();
+	}
 
 }
