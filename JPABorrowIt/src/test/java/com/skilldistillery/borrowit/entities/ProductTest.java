6a674a7a06f334aea17dec842a6ab70261813bee
Khiveley <kenneth.hiveley@gmail.com>
2021-10-28 10:59:07 -0500

    Updated User

--- JPABorrowIt/src/test/java/com/skilldistillery/borrowit/entities/ProductTest.java
+++ JPABorrowIt/src/test/java/com/skilldistillery/borrowit/entities/ProductTest.java
@@ -2,6 +2,7 @@ package com.skilldistillery.borrowit.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,7 +15,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ProductTest {
-	
+
 	private static EntityManagerFactory emf;
 	private EntityManager em;
 	private Product product;
@@ -43,6 +44,7 @@ class ProductTest {
 
 	@Test
 	void test() {
+//		SELECT * from product where id = 1;
 //		+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+
 //		| id | title    | description                                                            | image_url                                                      | category_id |
 //		+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+
@@ -50,9 +52,60 @@ class ProductTest {
 
 		assertNotNull(product);
 		assertEquals("Monopoly", product.getTitle());
-		assertEquals("A family friendly game that most definitely won't end any friendships!", product.getDescription());
+		assertEquals("A family friendly game that most definitely won't end any friendships!",
+				product.getDescription());
 		assertEquals("https://m.media-amazon.com/images/I/91RSg9MCGtL._AC_SY450_.jpg", product.getImageUrl());
 		assertEquals(1, product.getCategory().getId());
 	}
+	
+	@Test
+	
+	void test_OneToMany_product_to_productItems() {
+		
+//		select *  from product p  join product_item i on p.id = i.product_id where p.id = 1;
+//		+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+----+---------+------------+---------+---------+
+//		| id | title    | description                                                            | image_url                                                      | category_id | id | quality | product_id | user_id | version |
+//		+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+----+---------+------------+---------+---------+
+//		|  1 | Monopoly | A family friendly game that most definitely won't end any friendships! | https://m.media-amazon.com/images/I/91RSg9MCGtL._AC_SY450_.jpg |           1 |  1 | Mint    |          1 |       1 | Classic |
+//		+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+----+---------+------------+---------+---------+
+		
+		assertNotNull(product);
+		assertNotNull(product.getProductItems());
+		assertTrue(product.getProductItems().size()>0);
+		assertEquals(1, product.getProductItems().size());
+	}
 
+	@Test
+	
+	void test_ManyToOne_Product_to_Category() {
+		
+//		select * from product p join category c on p.id = c.id where p.id = 1;
+//		+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+----+------------+
+//		| id | title    | description                                                            | image_url                                                      | category_id | id | category   |
+//		+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+----+------------+
+//		|  1 | Monopoly | A family friendly game that most definitely won't end any friendships! | https://m.media-amazon.com/images/I/91RSg9MCGtL._AC_SY450_.jpg |           1 |  1 | Board Game |
+//		+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+----+------------+
+		
+		assertNotNull(product);
+		assertNotNull(product.getCategory());
+		assertEquals(1, product.getCategory().getId());
+	}
+	
+	@Test
+	
+	void test_OneToMany_Product_to_Rating() {
+		
+//		select * from rating r join product p on p.id = r.product_id where r.product_id = 1;
+//		+--------+----------------+---------+------------+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+
+//		| rating | rating_comment | user_id | product_id | id | title    | description                                                            | image_url                                                      | category_id |
+//		+--------+----------------+---------+------------+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+
+//		|      4 | NULL           |       1 |          1 |  1 | Monopoly | A family friendly game that most definitely won't end any friendships! | https://m.media-amazon.com/images/I/91RSg9MCGtL._AC_SY450_.jpg |           1 |
+//		+--------+----------------+---------+------------+----+----------+------------------------------------------------------------------------+----------------------------------------------------------------+-------------+
+
+		assertNotNull(product);
+		assertNotNull(product.getRatings());
+		assertTrue(product.getRatings().size()>0);
+		assertEquals(1, product.getRatings().size());
+	}
+	
 }
