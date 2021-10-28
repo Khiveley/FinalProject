13a3548c03a540296fdc010a64984d9bee22e087
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-10-28 15:05:58 -0600

    save changes for borrow controller files

---
+++ BorrowIt/src/main/java/com/skilldistillery/borrowit/services/BorrowService.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.borrowit.services;
+
+import java.util.List;
+import java.util.Set;
+
+import com.skilldistillery.borrowit.entities.Borrow;
+
+public interface BorrowService {
+
+	List<Borrow> index(String name);
+
+	Borrow show(String name, int bid);
+
+	Borrow create(String name, Borrow borrow);
+
+	boolean destroy(String name, int bid);
+
+	Borrow update(String name, int bid, Borrow borrow);
+
+}
---
+++ BorrowIt/src/main/java/com/skilldistillery/borrowit/services/BorrowServiceImpl.java
@@ -0,0 +1,48 @@
+package com.skilldistillery.borrowit.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.borrowit.entities.Borrow;
+import com.skilldistillery.borrowit.repositories.BorrowRepository;
+
+
+@Service
+public class BorrowServiceImpl implements BorrowService{
+	
+	@Autowired
+	private BorrowRepository borrowRepo;
+
+	@Override
+	public List<Borrow> index(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Borrow show(String name, int bid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Borrow create(String name, Borrow borrow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean destroy(String name, int bid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Borrow update(String name, int bid, Borrow borrow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
