79813bedda9dcbc1b3fce0f7f0c07a647b91c644
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-10-29 10:26:30 -0600

    ProdList

--- BorrowIt/src/main/java/com/skilldistillery/borrowit/services/ProductItemServiceImpl.java
+++ BorrowIt/src/main/java/com/skilldistillery/borrowit/services/ProductItemServiceImpl.java
@@ -1,5 +1,63 @@
 package com.skilldistillery.borrowit.services;
 
-public class ProductItemServiceImpl implements ProductItemService{
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.borrowit.entities.ProductItem;
+import com.skilldistillery.borrowit.repositories.ProductItemRepository;
+
+@Service
+public class ProductItemServiceImpl implements ProductItemService{
+	
+	@Autowired
+	private ProductItemRepository repo;
+
+	@Override
+	public ProductItem getProductItemById(Integer id) {
+		Optional<ProductItem> prodOpt = repo.findById(id);
+		if (prodOpt.isPresent()) {
+			return prodOpt.get();
+		}
+		return null;
+	}
+
+	@Override
+	public List<ProductItem> getAll() {
+		return repo.findAll();
+	}
+
+	@Override
+	public List<ProductItem> getAllAvailableProductItems() {
+		return repo.findbyBorrowed(false);
+	}
+
+	@Override
+	public ProductItem createProductItem(ProductItem productItem) {
+		repo.save(productItem);
+		return productItem;
+	}
+
+	@Override
+	public ProductItem updateProductItem(int id, ProductItem productItem) {
+		Optional<ProductItem> prodOpt = repo.findById(id);
+		if (prodOpt.isPresent()) {
+			ProductItem managedProductItem = prodOpt.get();
+			managedProductItem.setProduct(productItem.getProduct());
+			managedProductItem.setQuality(productItem.getQuality());
+			managedProductItem.setVersion(productItem.getVersion());
+			managedProductItem.setUser(productItem.getUser());
+			repo.saveAndFlush(productItem);
+			return managedProductItem;
+		}
+		return productItem;
+	}
 }
+
+
+
+
+
+
