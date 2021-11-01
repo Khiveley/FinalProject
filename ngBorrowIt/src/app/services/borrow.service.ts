040144faf77bb01a8be3578be3e2dbd1a9dcf1f4
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-11-01 15:01:31 -0600

    service update save

--- ngBorrowIt/src/app/services/borrow.service.ts
+++ ngBorrowIt/src/app/services/borrow.service.ts
@@ -21,75 +21,68 @@ export class BorrowService {
     private auth: AuthService
   ) {}
 
-  // index(): Observable<Borrow[]> {
-  //   return this.http.get<Borrow[]>(this.url, this.getHttpOptions()).pipe(
-  //     catchError((err: any) => {
-  //       console.log(err);
-  //       return throwError(
-  //         'BorrowService.index(): Error retrieving borrow list'
-  //       );
-  //     })
-  //   );
-  // }
+  index(): Observable<Borrow[]> {
+    return this.http.get<Borrow[]>(this.url, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          'BorrowService.index(): Error retrieving borrow list'
+        );
+      })
+    );
+  }
 
-  // show(todoId: number): Observable<Borrow> {
-  //   return this.http
-  //     .get<Borrow>(this.url + '/' + Borrow, this.getHttpOptions())
-  //     .pipe(
-  //       catchError((err: any) => {
-  //         console.log(err);
-  //         return throwError('BorrowService.show(): Error retrieving borrow');
-  //       })
-  //     );
-  // }
-  // create(borrow: Borrow): Observable<Borrow> {
-  //   borrow.completed = false;
-  //   borrow.description = ' ';
-  //   return this.http.post<Borrow>(this.url, borrow, this.getHttpOptions()).pipe(
-  //     catchError((err: any) => {
-  //       console.log(err);
-  //       return throwError('BorrowService.create(): Error creating borrow');
-  //     })
-  //   );
-  // }
+  show(todoId: number): Observable<Borrow> {
+    return this.http
+      .get<Borrow>(this.url + '/' + Borrow, this.getHttpOptions())
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('BorrowService.show(): Error retrieving borrow');
+        })
+      );
+  }
+  create(borrow: Borrow): Observable<Borrow> {
+    return this.http.post<Borrow>(this.url, borrow, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('BorrowService.create(): Error creating borrow');
+      })
+    );
+  }
 
-  // update(borrow: Borrow) {
-  //   if (borrow.completed) {
-  //     borrow.completeDate = this.datePipe.transform(Date.now(), 'shortDate'); // 8/24/1999
-  //   } else {
-  //     borrow.completeDate = ' ';
-  //   }
-  //   return this.http
-  //     .put<Borrow>(`${this.url}/${borrow.id}`, borrow, this.getHttpOptions())
-  //     .pipe(
-  //       catchError((err: any) => {
-  //         console.log(err);
-  //         return throwError('BorrowService.update(): Error creating borrow');
-  //       })
-  //     );
+  update(borrow: Borrow) {
+    return this.http
+      .put<Borrow>(`${this.url}/${borrow.id}`, borrow, this.getHttpOptions())
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('BorrowService.update(): Error creating borrow');
+        })
+      );
+  }
+
+  // this.editTodo = null;
+
+  destroy(id: number) {
+    return this.http.delete(`${this.url}/${id}`, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          'BorrowService.destory(): Error deleting borrow item'
+        );
+      })
+    );
+  }
+
+  getHttpOptions() {
+    let credentials = this.auth.getCredentials();
+    let options = {
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        Authorization: `Basic ${credentials}`,
+      },
+    };
+    return options;
+  }
 }
-
-// this.editTodo = null;
-
-// destroy(id: number) {
-//   return this.http.delete(`${this.url}/${id}`, this.getHttpOptions()).pipe(
-//     catchError((err: any) => {
-//       console.log(err);
-//       return throwError(
-//         'BorrowService.destory(): Error deleting borrow item'
-//       );
-//     })
-//   );
-// }
-
-// getHttpOptions() {
-//   let credentials = this.auth.getCredentials();
-//   let options = {
-//     headers: {
-//       'X-Requested-With': 'XMLHttpRequest',
-//       Authorization: `Basic ${credentials}`,
-//     },
-//   };
-//   return options;
-// }
-// }
