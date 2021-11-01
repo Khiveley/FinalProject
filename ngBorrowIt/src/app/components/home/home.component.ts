7811986fe2f139e7b474d028c8d5d90fe3268d2b
Khiveley <kenneth.hiveley@gmail.com>
2021-11-01 18:42:36 -0500

    updated registration

--- ngBorrowIt/src/app/components/home/home.component.ts
+++ ngBorrowIt/src/app/components/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { subscribeOn } from 'rxjs/operators';
 import { Category } from 'src/app/models/category';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -20,11 +23,44 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
-    private productService: ProductService
-
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    let productIdString = this.route.snapshot.paramMap.get('id');
+    if(productIdString){
+      let productId = Number.parseInt(productIdString);
+      if(!isNaN(productId)){
+        this.productService.show(productId).subscribe(
+          (product) => {
+            this.reloadProducts();
+            this.selected = product;
+          },
+          (error) => {
+            console.error('Invalid Product Id' + productId);
+            this.router.navigateByUrl('notfound');
+          }
+        );
+      } else {
+        console.error('Invalid Product ID' + productId);
+        this.router.navigateByUrl('notfound');
+      }
+    }
+    this.reloadProducts();
+  }
+
+  reloadProducts(): void {
+    this.productService.index().subscribe(
+      (products) => {
+        this.products = products;
+      },
+      (error) => {
+        console.error('Error retrieving products');
+        console.error(error);
+      }
+    );
   }
 
 }
