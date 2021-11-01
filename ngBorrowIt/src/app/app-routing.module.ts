75da4a6bd0c648b24510fc478cea409724141ea4
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-11-01 11:24:47 -0600

    Nav Bar and Home Page

--- ngBorrowIt/src/app/app-routing.module.ts
+++ ngBorrowIt/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutpageComponent } from './components/aboutpage/aboutpage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'home' },
+{ path: 'home', component: HomeComponent },
+{ path: 'aboutpage', component: AboutpageComponent }];
+// { path: '**', component: NotFoundComponent }
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- ngBorrowIt/src/app/app.component.ts
+++ ngBorrowIt/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
   title = 'ngBorrowIt';
+
+  products: Product[] = [ ];
+
 }
+
